52e8cb65a7d82d9ed6440dce16d2ff994e28220b
Capra <youribexcapra@capra>
2022-09-04 20:21:50 -0400

    testing commit changes...

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World! v1.0.0");
+﻿namespace RubiX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! v1.0.0");
+            if (args.Length > 0) foreach (string arg in args) Console.WriteLine(arg);
+        }
+    }
+}
