1cccc6ecaa76ad56eed059fec288a7ac94462e64
Capra <youribexcapra@capra>
2022-09-11 21:16:20 -0400

    Removed some utils classes and added IO and Logger.

--- Program.cs
+++ Program.cs
@@ -7,14 +7,35 @@ namespace RubiX
         static void Main(string[] args)
         {
             // Print logo.
-            Console.WriteLine(Globals._logo);
+            IO.Logo();
 
             // Handle CMD args.
             if (args.Length > 0) foreach (string arg in args) Console.WriteLine(arg);
 
-            // Take user input.
-            string input = Input.input();
+            while (true)
+            {
+                // Take user input.
+                string input = IO.In();
 
+                switch (input)
+                {
+                    case "exit":
+                        break;
+
+                    case "ls":
+                        Console.WriteLine("hello");
+                        continue;
+
+                    default:
+                        IO.Out("\nInvalid command: ", ConsoleColor.Yellow);
+                        IO.Out(input + "\n", ConsoleColor.Red);
+                        continue;
+                }
+
+                break;
+            }
+
+            IO.Out("\nDone!\n\n", ConsoleColor.Green);
         }
     }
 }
